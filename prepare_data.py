643c0514268d87112ce24b429b3c8d17898ad17f
Guilherme <sitegui@sitegui.com.br>
2019-09-10 23:19:43 +0200

    Split train/validation/test

--- prepare_data.py
+++ prepare_data.py
@@ -3,10 +3,32 @@ from zipfile import ZipFile
 from sklearn.model_selection import train_test_split
 from PIL import Image, ImageOps
 import numpy as np
+import pandas as pd
 
-VALIDATION_RATIO = 0.2
-VALIDATION_CLASSES = ['cat', 'dog']
+VALIDATION_RATIO = 0.3
+TEST_RATIO = 0.1
+TEST_CLASSES = ['cat', 'dog']
 TARGET_SIZE = (128, 128)
+
+
+def split_sets(elements, ratios):
+    """
+    Split a list into sets (randomly, but deterministically)
+    :param elements: a list with N elements
+    :param ratios: a list with K-1 floats that must sum less or equal to 1
+    :returns: a list with K lists of elements
+    """
+    quantities = np.floor(np.array(ratios) * len(elements)).astype('int')
+    np.random.seed(17)
+    np.random.shuffle(elements)
+    result = []
+    last_index = 0
+    for quantity in quantities:
+        result.append(elements[last_index:last_index+quantity])
+        last_index += quantity
+    result.append(elements[last_index:])
+    return result
+
 
 if not os.path.exists('data/natural-images.zip'):
     raise Exception(
@@ -18,61 +40,55 @@ if not os.path.exists('data/natural_images'):
     ZipFile('data/natural-images.zip').extractall('data')
 
 if not os.path.exists('data/training'):
-    print('=== Prepare validation and training sets ===')
+    print('=== Prepare training, validation and test sets ===')
 
-    # Load all files (sorted by name)
-    files_by_class = {}
+    # Load all files and split into sets
+    files_by_destination = {}
     for class_dir in os.scandir('data/natural_images'):
-        files_by_class[os.path.basename(class_dir.path)] = sorted([
-            img_file.path for img_file in os.scandir(class_dir.path)
-        ])
-
-    # Split training/validation
-    training_by_class = {}
-    validation_by_class = {}
-    for class_name, files in files_by_class.items():
-        if class_name not in VALIDATION_CLASSES:
-            training_by_class[class_name] = files
+        class_name = os.path.basename(class_dir.path)
+        file_names = sorted(
+            img_file.path for img_file in os.scandir(class_dir.path))
+        if class_name in TEST_CLASSES:
+            # train/validation/test
+            a, b, c = split_sets(file_names, [VALIDATION_RATIO, TEST_RATIO])
+            files_by_destination[f'data/validation/{class_name}'] = a
+            files_by_destination[f'data/test/{class_name}'] = b
+            files_by_destination[f'data/training/{class_name}'] = c
         else:
-            training_by_class[class_name], validation_by_class[class_name] = train_test_split(files, test_size=VALIDATION_RATIO, random_state=17, shuffle=True)
-    
-    # Crop and copy files
-    def treat_files(files_by_class, subset):
-        for class_name, files in files_by_class.items():
-            dir_destination = f'data/{subset}/{class_name}'
-            print(f'- Copy preprocessed images to {dir_destination}')
-            if not os.path.exists(dir_destination):
-                os.makedirs(dir_destination)
-            for a_file in files:
-                img = Image.open(a_file)
+            # train/validation
+            a, b = split_sets(file_names, [VALIDATION_RATIO])
+            files_by_destination[f'data/validation/{class_name}'] = a
+            files_by_destination[f'data/training/{class_name}'] = b
 
-                # Fit image
-                k_w = TARGET_SIZE[0] / img.width
-                k_h = TARGET_SIZE[1] / img.height
-                k = min(k_w, k_h)
-                w = round(img.width * k)
-                h = round(img.height * k)
-                img = img.resize((w, h), Image.BICUBIC)
-                assert img.width == TARGET_SIZE[0] or img.height == TARGET_SIZE[1]
+    for destination, files in files_by_destination.items():
+        print(f'- Preprocess {len(files)} images to {destination}')
+        if not os.path.exists(destination):
+            os.makedirs(destination)
+        for a_file in files:
+            img = Image.open(a_file)
 
-                # Pad image with repeating pixels
-                # (using dark pixels disturb the CNN)
-                array = np.array(img)
-                delta_w = TARGET_SIZE[0] - img.width
-                delta_h = TARGET_SIZE[1] - img.height
-                padding = (
-                    (delta_h // 2, delta_h - delta_h // 2),
-                    (delta_w // 2, delta_w - delta_w // 2),
-                    (0, 0)
-                )
-                array = np.pad(array, padding, 'edge')
-                img = Image.fromarray(array)
-                assert img.width == TARGET_SIZE[0] and img.height == TARGET_SIZE[1]
+            # Fit image
+            k_w = TARGET_SIZE[0] / img.width
+            k_h = TARGET_SIZE[1] / img.height
+            k = min(k_w, k_h)
+            w = round(img.width * k)
+            h = round(img.height * k)
+            img = img.resize((w, h), Image.BICUBIC)
+            assert img.width == TARGET_SIZE[0] or img.height == TARGET_SIZE[1]
 
-                # Save image
-                img.save(f'{dir_destination}/{os.path.basename(a_file)}')
+            # Pad image with repeating pixels
+            # (using dark pixels disturb the CNN)
+            array = np.array(img)
+            delta_w = TARGET_SIZE[0] - img.width
+            delta_h = TARGET_SIZE[1] - img.height
+            padding = (
+                (delta_h // 2, delta_h - delta_h // 2),
+                (delta_w // 2, delta_w - delta_w // 2),
+                (0, 0)
+            )
+            array = np.pad(array, padding, 'edge')
+            img = Image.fromarray(array)
+            assert img.width == TARGET_SIZE[0] and img.height == TARGET_SIZE[1]
 
-    treat_files(training_by_class, 'training')
-    treat_files(validation_by_class, 'validation')
-    
-    
+            # Save image
+            img.save(f'{destination}/{os.path.basename(a_file)}')
